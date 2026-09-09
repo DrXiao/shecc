35b21a95c903bdf038af826cb98f893bd455d331
Yu-En Hsiao <drxiao6399@gmail.com>
2026-09-09 21:08:31 +0800

    Implement immediate binding for dynamic linking
    Since the original codebase already implemented the immediate binding
    flag for the x86-64 and AArch64 architectures, this commit introduces a
    "-z" option so that the Arm32 and RV32 targets can also use immediate
    binding.
    
    When the command-line arguments contain "-z now", the compiler sets its
    internal flag (imm_binding) to true and generates the
    Elf32_Dyn/Elf64_Dyn objects to enable exectuables to perform immediate
    binding at runtime.
    
    For 64-bit targets, immediate binding is always used under the dynamic
    linking mode as is.

--- src/main.c
+++ src/main.c
@@ -121,7 +121,18 @@ int main(int argc, char *argv[])
             libc = false;
         else if (!strcmp(argv[i], "--dynlink"))
             dynlink = true;
-        else if (!strcmp(argv[i], "-E"))
+        else if (!strcmp(argv[i], "-z")) {
+            if (i + 1 >= argc)
+                usage_error("-z requires \"lazy\" or \"now\"");
+
+            if (!strcmp(argv[i + 1], "lazy"))
+                imm_binding = false;
+            else if (!strcmp(argv[i + 1], "now"))
+                imm_binding = true;
+            else
+                usage_error("-z requires \"lazy\" or \"now\"");
+            i++;
+        } else if (!strcmp(argv[i], "-E"))
             expand_only = true;
         else if (!strcmp(argv[i], "-I")) {
             if (i + 1 >= argc)
@@ -145,11 +156,22 @@ int main(int argc, char *argv[])
             in = argv[i];
     }
 
+    if (dynlink) {
+        switch (ELF_MACHINE) {
+        /* The following 64-bit targets have no lazy-resolution path, so
+         * immediate binding must be used.
+         */
+        case ELF_MACHINE_X86_64:
+        case ELF_MACHINE_AARCH64:
+            imm_binding = true;
+        }
+    }
+
     if (!in) {
         printf(
             "Usage: shecc [-I directory] [-o output] [+m] [--dot] [--dump-ir] "
             "[--warn-string-literals] [--std=c99] [--no-libc] "
-            "[--dynlink] [-E] <input.c>\n");
+            "[--dynlink] [-z <lazy | now>] [-E] <input.c>\n");
         usage_error("Missing source file");
     }
 
